cf190e70639ef0ef49a25b9812f6ed5cf3b4dcbd
BlackBear <111135227+BllackBear@users.noreply.github.com>
2022-08-12 16:41:38 +0200

    Add files via upload

---
+++ bb_watermark/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "bb_watermark"
+description "Simple watermark on-screen"
+author "BlackBear"
+version "1.0.0"
+lua54 'yes'
+
+
+ui_page 'nui.html'
+
+files {
+    'style.css',
+    'script.js',
+    'images',
+}
